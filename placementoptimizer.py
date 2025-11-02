d79788adf92850d42b662cd85359c08712edad5b
Laimis Juzeliūnas <58551069+laimis9133@users.noreply.github.com>
2025-11-02 18:47:27 +0100

    Update placementoptimizer.py
    Fixed import typo

--- placementoptimizer.py
+++ placementoptimizer.py
@@ -5523,7 +5523,6 @@ def repairstats(args, cluster):
 
 
 def main():
-    import io
     if sys.stdout.encoding.lower() != 'utf-8':
         sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
         sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8')
